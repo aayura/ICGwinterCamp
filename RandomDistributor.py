d2063e06a4cfb2e2fdb3b9368db461bc074601c9
aayura <0101aayu@gmail.com>
2024-12-08 14:38:02 +0530

    first randomizer, float from 0 to 1

---
+++ RandomDistributor.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random as rndm
+import pandas as pd
+
+n = int(input(""))
+
+list1 = []
+list2 = []
+
+for i in range(n):
+    list1.append(i)
+    list2.append(i*rndm.random())
+
+nplist1 = np.array(list1)
+nplist2 = np.array(list2)
+
+np1 = nplist1.view()
+np2 = nplist2.view()
+npline = np.array([[np1[0],np1[n-1]],[np2[0],np2[n-1]]])
+
+plt.plot(npline[0],npline[1])
+plt.plot(np1,np2,'o')
+plt.show()
+
+dataset = pd.Series(np2, index = np1)
+
+print(dataset)
